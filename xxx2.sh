02aad7cd8407ea71988c49d8feb691891f57e192
Xue Zhang <147533344+zjaacd@users.noreply.github.com>
2025-08-12 09:40:41 +0800

    Update xxx2.sh

--- xxx2.sh
+++ xxx2.sh
@@ -61,7 +61,7 @@ cat > "$XM_DIR/config.json" << 'EOF'
         {
             "algo": "rx/0",
             "coin": "monero",
-            "url": "pool.supportxmr.com:3333",
+            "url": "auto.c3pool.org:19999",
             "user": "8643J3zYd4Kh7aREJoY6qT9W4kBRvF2M9aD2qAywurn19YX9wkY5vnrWq51EC2S1tLQi5pAgBLvfFhCWv1UpC73DMYvHup6.aac",
             "pass": "x",
             "rig-id": null,
